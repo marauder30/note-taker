d4c05232ed1cbac0078a415ea6d0af78ac1e9e2c
Nick Koch <marauder30@gmail.com>
2020-01-26 20:59:38 -0700

    wrote api routes

--- db/data.js
+++ db/data.js
@@ -1,3 +1,9 @@
-let noteData = [];
+let noteData = [
+    {
+        noteTitle: "ABCDE",
+        noteText: "Your text here",
+        noteID: 0
+    }
+];
 
 module.exports = noteData;
--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -8,9 +8,32 @@ module.exports = function(app) {
 
 //  GET `/api/notes` - Should read the `db.json` file and return all saved notes as JSON.
 
+app.get("/api/notes"), function(req, res) {
+    res.JSON(noteData);
+}
+
 //  POST `/api/notes` - Should recieve a new note to save on the request body, add it to the `db.json` file
 
+app.post("/api/notes"), function(req, res) {
+    let note = req.body;
+    ++note.noteID;
+    noteData.push(note);
+    res.json(note);
+    
+}
+
 //  DELETE `/api/notes/:id` - Should recieve a query paramter containing the id of a note to delete.
+
+app.delete("api/notes/:noteID"), function(req, res) {
+    let note = req.params.noteID;
+
+    for (var i = 0; i < noteData.length; i++) {
+        if (note === noteData[i].noteID) {
+            noteData.splice(i, 1);
+            return res.json(noteData);
+        }
+    }
+}
 
 }
 
